6ec6962bc5f0dee28bd820e14b0fa4f4ec6b47d0
OlegPalchyk <olegpa@interlink-ua.com>
2015-11-17 11:44:24 +0200

    Initial commit

---
+++ src/main/java/com/mkyong/users/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.mkyong.users.dao;
+
+import com.mkyong.users.model.User;
+
+public interface UserDao {
+
+    User findByUserName(String username);
+
+}
+
---
+++ src/main/java/com/mkyong/users/model/UserRole.java
@@ -0,0 +1,35 @@
+package com.mkyong.users.model;
+
+public class UserRole{
+
+    public Integer getUserRoleId() {
+        return userRoleId;
+    }
+
+    public void setUserRoleId(Integer userRoleId) {
+        this.userRoleId = userRoleId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    private Integer userRoleId;
+    private User user;
+    private String role;
+
+    //getter and setter methods
+}
+
